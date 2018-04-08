0dca8473fc968f63bf0eff18cc2dd9e37b945df5
lachlan <lachlan.deer@uzh.ch>
2018-04-08 16:47:10 +0200

    build jets data to RDS

---
+++ data-raw/jets.R
@@ -0,0 +1,25 @@
+# tidy up jets data
+
+library(magrittr)
+library(dplyr)
+
+jets <- readr::read_delim("data-raw/jets.txt", delim = "\t", col_names = FALSE) %>%
+            select(-X9)
+
+jets <- jets %>%
+            rename(model_id = X1,
+                   manufacturer_id = X2,
+                   year = X3,
+                   price = X4,
+                   quantity = X5,
+                   max_range = X6,
+                   speed = X7,
+                   cabin_volume = X8)
+
+manu_ids <- c(1,2,3)
+brands <- c("Bombardier", "Dassault", "Gulfstream")
+
+brand_data <- data.frame(manu_ids, brands)
+
+jets <- jets %>%
+            left_join(brand_data, by = c("manufacturer_id" = "manu_ids"))
