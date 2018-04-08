4d75012827cc5bca6c79203907fd59ee39439216
lachlan <lachlan.deer@uzh.ch>
2018-04-08 16:56:08 +0200

    add analgesics to r package

---
+++ data-raw/analgesics.R
@@ -0,0 +1,17 @@
+# tidy up analgesics data
+
+library(magrittr)
+library(dplyr)
+
+analgesics <- readr::read_delim("data-raw/analgesics.csv", delim = "\t")
+
+analgesics <- analgesics %>%
+                rename(store_id = store,
+                       brand_id = brand,
+                       sales = sales_,
+                       customers = count,
+                       price = price_,
+                       promption = prom_,
+                       wholesale_price = cost_)
+
+devtools::use_data(analgesics, overwrite = TRUE, compress = 'xz')
