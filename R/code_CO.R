33337da12e2e1d40e02100c485f11d956176e7d8
Christian Oertel <christianoertel.co@gmail.com>
2018-04-09 16:44:39 +0200

    function deals with list containing just names

--- R/code_CO.R
+++ R/code_CO.R
@@ -1,24 +1,25 @@
 
 ### --- Generate aggregate market shares per nest and subnest
 
-gen_nest_shares <- function(mkt_shares,market_id,nest_id){
-  t_id <- market_id[["time_id"]]
-  geo_id <- market_id[["geog_id"]]
+gen_nest_shares <- function(df,mkt_shares,market_id,nest_id){
+  t_id <- df[[market_id[["time_id"]]]]
+  geo_id <- df[[market_id[["geog_id"]]]]
   nest_shares <- aggregate(mkt_shares, by=list(t_id=t_id,geo_id=geo_id,nest_id=nest_id), FUN=sum)
     # sum individual-observation shares by nest within markets
   obs <- left_join(as.data.frame(cbind(t_id,geo_id,nest_id,deparse.level = 1)),nest_shares,by=c("t_id","geo_id","nest_id"))
-    # expand sum of ahres to match number of obs in data
+    # expand sum of shares to match number of obs in data
   nest_shares <- obs[[ncol(nest_shares)]]
     # export numerical vector of aggregate nest shares
 }
 
-gen_subnest_shares <- function(mkt_shares,market_id,nest_id,subnest_id){
-  t_id <- market_id[["time_id"]]
-  geo_id <- market_id[["geog_id"]]
+gen_subnest_shares <- function(df,mkt_shares,market_id,nest_id,subnest_id){
+  t_id <- df[[market_id[["time_id"]]]]
+  geo_id <- df[[market_id[["geog_id"]]]]
   subnest_shares <- aggregate(mkt_shares, by=list(t_id=t_id,geo_id=geo_id,nest_id=nest_id,subnest_id=subnest_id), FUN=sum)
   obs <- left_join(as.data.frame(cbind(t_id,geo_id,nest_id,subnest_id,deparse.level = 1)),subnest_shares,by=c("t_id","geo_id","nest_id","subnest_id"))
   subnest_shares <- obs[[ncol(subnest_shares)]]
 }
+
 
 ### --- Generate logged nest and subnest share ratios
 
