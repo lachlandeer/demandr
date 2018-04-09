6af63d6964c58ea73b50d1947403af16c6219341
lachlan <lachlan.deer@uzh.ch>
2018-04-09 17:25:40 +0200

    create outside shares - the correct way

--- R/create_shares.R
+++ R/create_shares.R
@@ -1,55 +1,19 @@
-compute_outside_share <- function(mkt_shares, market_id, data_frame) {
-
-    # use pipe without loading package
-    `%>%` <- magrittr::`%>%`
-
-    #if (is.null(market_id[["geog_id"]]) == FALSE) {
-        print(paste("Grouping by Geographic ID:", market_id[["geog_id"]],
-            sep = " "))
-    #    }
-    #if (is.null(market_id[["geog_id"]]) == FALSE ) {
-        print(paste("Grouping by Time ID      :", market_id[["time_id"]],
-            sep = " "))
-    #    }
-
-    # unpack market_ids to a vector so can group by easily
-    markets <- unlist(market_id)
-
-    df <- data_frame
-
-    # generate outside shares
-    df <- df %>%
-            dplyr::group_by(!!! rlang::syms(markets)) %>%
-            dplyr::mutate(inside_share = sum(!!! rlang::sym(mkt_shares))) %>%
-            dplyr::mutate(outside_share = 1 - inside_share) %>%
-            # clean up columns we don't want returned
-            dplyr::select(-inside_share)
-            #dplyr::select(-inside_share, -geog_id, -time_id)
-            ## TODO: add asserts that shares are between zero and one
-
-    return(df)
-}
-
-
-compute_outside_share2 <- function(df, mkt_shares, market_id) {
+compute_outside_share <- function(df, mkt_shares, market_id) {
 
     # use pipe without loading package
     `%>%` <- magrittr::`%>%`
 
     # unpack market_ids to a vector so can group by easily
     markets <- unlist(market_id)
-
-    #df <- data_frame
 
     # generate outside shares
     outside_share <- df %>%
             dplyr::group_by(!!! rlang::syms(markets)) %>%
             dplyr::mutate(inside_share = sum(!!! rlang::sym(mkt_shares))) %>%
             dplyr::mutate(outside_share = 1 - inside_share) %>%
-            # clean up columns we don't want returned
-            #ungroup() %>%
-            #dplyr::select(outside_share)
+            # ungroup the data and clean up columns we don't want returned
+            ungroup() %>%
+            dplyr::select(-inside_share, -geog_id, -time_id)
             ## TODO: add asserts that shares are between zero and one
 
-    #return(outside_share)
 }
