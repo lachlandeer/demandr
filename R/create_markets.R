b5975e60ebc2a4932d2cdda00a9bccb79bfd876c
lachlan <lachlan.deer@uzh.ch>
2018-04-09 12:32:24 +0200

    creates an object markets that identifies what variables define a market in a dataset

---
+++ R/create_markets.R
@@ -0,0 +1,11 @@
+# this function maps market ids into a list so they are
+#consistently unpacked
+
+create_markets <- function(geog_id, time_id){
+    markets <- list()
+
+    markets[["geog_id"]] <- geog_id
+    markets[["time_id"]] <- time_id
+
+    return(markets)
+}
