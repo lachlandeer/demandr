b0a0d9801ee70b3edd8aae05037a6e4f072375dc
lachlan <lachlan.deer@uzh.ch>
2018-04-09 14:34:31 +0200

    function to create outside shares

--- R/create_shares.R
+++ R/create_shares.R
@@ -1,12 +1,24 @@
+library(dplyr)
+
 compute_outside_share <- function(mkt_shares, market_id, data_frame) {
-    # expect mkt_shares = c("something")
-    # market_id = c("something", "something2")
-    # df <- data_frame
-    #
-    # df %>%
-    #     group_by(year, country) %>%
-    #     mutate(inside_share = sum(mkt_share)) %>%
-    #     mutate(outside_share = 1-inside_share)
 
+    print(paste("Grouping by Geographic ID:", market_id[["geog_id"]],
+            sep = " "))
+    print(paste("Grouping by Time ID      :", market_id[["time_id"]],
+            sep = " "))
 
+    # unpack market_ids to a vector so can group by easily
+    markets <- unlist(market_id)
+
+    df <- data_frame
+
+    # generate outside shares
+    df <- df %>%
+            dplyr::group_by(!!! rlang::syms(markets)) %>%
+            dplyr::mutate(inside_share = sum(!!! rlang::sym(mkt_shares))) %>%
+            dplyr::mutate(outside_share = 1 - inside_share) %>%
+            # clean up columns we don't want returned
+            dplyr::select(-inside_share, -geog_id, -time_id)
+
+    return(df)
 }
