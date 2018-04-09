9fec49e2e4e13653549f3f2711d9fbc42a0f1a6e
lachlan <lachlan.deer@uzh.ch>
2018-04-09 12:25:14 +0200

    draft function to compute outside shares

---
+++ R/create_shares.R
@@ -0,0 +1,12 @@
+compute_outside_share <- function(mkt_shares, market_id, data_frame) {
+    # expect mkt_shares = c("something")
+    # market_id = c("something", "something2")
+    # df <- data_frame
+    #
+    # df %>%
+    #     group_by(year, country) %>%
+    #     mutate(inside_share = sum(mkt_share)) %>%
+    #     mutate(outside_share = 1-inside_share)
+
+
+}
