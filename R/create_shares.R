5f7c314755f95a25d173b330ae574ef1cd09b2c6
lachlan <lachlan.deer@uzh.ch>
2018-04-16 16:30:37 +0200

    remove all plurals in of share

--- R/create_shares.R
+++ R/create_shares.R
@@ -1,5 +1,5 @@
 ## documentation needed
-compute_outside_share <- function(df, mkt_shares, market_id) {
+compute_outside_share <- function(df, mkt_share, market_id) {
 
     # use pipe without loading package
     `%>%` <- magrittr::`%>%`
@@ -7,20 +7,20 @@ compute_outside_share <- function(df, mkt_shares, market_id) {
     # unpack market_ids to a vector so can group by easily
     markets <- unlist(market_id)
 
-    # generate outside shares
+    # generate outside share
     outside_share <- df %>%
             dplyr::group_by(!!! rlang::syms(markets)) %>%
-            dplyr::mutate(inside_share = sum(!!! rlang::sym(mkt_shares))) %>%
+            dplyr::mutate(inside_share = sum(!!! rlang::sym(mkt_share))) %>%
             dplyr::mutate(outside_share = 1 - inside_share) %>%
             # ungroup the data and clean up columns we don't want returned
             ungroup() %>%
             dplyr::select(-inside_share)
-            ## TODO: add asserts that shares are between zero and one
+            ## TODO: add asserts that share are between zero and one
 
 }
 
-## dplyr approach to nest shares (testing)
-gen_nest_shares <- function(df, mkt_shares, market_id, nest_id){
+## dplyr approach to nest share (testing)
+gen_nest_share <- function(df, mkt_share, market_id, nest_id){
     # use pipe without loading package
     `%>%` <- magrittr::`%>%`
 
@@ -29,14 +29,14 @@ gen_nest_shares <- function(df, mkt_shares, market_id, nest_id){
 
     mkt_and_nests <- c(markets, nest_id)
 
-    nest_shares <- df %>%
+    nest_share <- df %>%
             dplyr::group_by(!!! rlang::syms(mkt_and_nests)) %>%
-            dplyr::mutate(nest_shares = sum(!!! rlang::sym(mkt_shares))) %>%
+            dplyr::mutate(nest_share = sum(!!! rlang::sym(mkt_share))) %>%
             ungroup()
 }
 
-## dplyr approach to subnest shares (testing)
-gen_subnest_shares <- function(df, mkt_shares, market_id, nest_id, subnest_id){
+## dplyr approach to subnest share (testing)
+gen_subnest_share <- function(df, mkt_share, market_id, nest_id, subnest_id){
     # use pipe without loading package
     `%>%` <- magrittr::`%>%`
 
@@ -45,26 +45,26 @@ gen_subnest_shares <- function(df, mkt_shares, market_id, nest_id, subnest_id){
 
     mkt_and_nests <- c(markets, nest_id, subnest_id)
 
-    nest_shares <- df %>%
+    nest_share <- df %>%
             dplyr::group_by(!!! rlang::syms(mkt_and_nests)) %>%
-            dplyr::mutate(subnest_shares = sum(!!! rlang::sym(mkt_shares))) %>%
+            dplyr::mutate(subnest_share = sum(!!! rlang::sym(mkt_share))) %>%
             ungroup()
 }
 
-gen_within_shares <- function(df, mkt_share, nest_share, subnest_share = NULL){
+gen_within_share <- function(df, mkt_share, nest_share, subnest_share = NULL){
     # if one level of nesting
     if(is.null(subnest_share)){
-        within_shares_one_nest(df, mkt_share, nest_share)
+        within_share_one_nest(df, mkt_share, nest_share)
     }
 
     # if two levels of nesting
     if(!is.null(subnest_share)){
-        within_shares_two_nest(df, mkt_share, nest_share, subnest_share)
+        within_share_two_nest(df, mkt_share, nest_share, subnest_share)
     }
 } # eof
 
-# shares for two level nests
-within_shares_two_nest <- function(df, mkt_share, nest_share, subnest_share){
+# share for two level nests
+within_share_two_nest <- function(df, mkt_share, nest_share, subnest_share){
     output <- df %>%
                 mutate(within_subnest = (!!rlang::sym(mkt_share)) /
                                             (!!rlang::sym(subnest_share)),
@@ -74,8 +74,8 @@ within_shares_two_nest <- function(df, mkt_share, nest_share, subnest_share){
     return(output)
 }
 
-# shares for one level nests
-within_shares_one_nest <- function(df, mkt_share, nest_share){
+# share for one level nests
+within_share_one_nest <- function(df, mkt_share, nest_share){
     output <- df %>%
                 mutate(
                        within_nest    = (!!rlang::sym(mkt_share)) /
@@ -84,26 +84,26 @@ within_shares_one_nest <- function(df, mkt_share, nest_share){
     return(output)
 }
 
-create_shares <- function(){
+create_share <- function(){
 
     # generate outside share if needed
     if(is.null(outside_share)){
         df <- df %>%
-                compute_outside_share(., mkt_shares, market_id)
+                compute_outside_share(., mkt_share, market_id)
     }
 
-    # generate nest shares for one level nest
+    # generate nest share for one level nest
     if(!is.null(nest_id) && is.null(subnest_id)){
         df <- df %>%
-                gen_nest_shares(., mkt_share, market_id, nest_id) %>%
-                gen_within_shares(., mkt_share, nest_share, subnest_share)
+                gen_nest_share(., mkt_share, market_id, nest_id) %>%
+                gen_within_share(., mkt_share, nest_share, subnest_share)
     }
 
     if(!is.null(nest_id) && !is.null(subnest_id)){
         df <- df %>%
-                gen_nest_shares(., mkt_share, market_id, nest_id) %>%
-                gen_subnest_shares(., mkt_shares, market_id, nest_id, subnest_id)
-                gen_within_shares(., mkt_share, nest_share, subnest_share)
+                gen_nest_share(., mkt_share, market_id, nest_id) %>%
+                gen_subnest_share(., mkt_share, market_id, nest_id, subnest_id)
+                gen_within_share(., mkt_share, nest_share, subnest_share)
     }
 
     return(df)
