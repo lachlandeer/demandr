44bdee3d96a30e3f16fea1fccce6a012a14ebf21
lachlan <lachlan.deer@uzh.ch>
2018-04-16 18:10:31 +0200

    shell of main estimation function

---
+++ R/estimate-demand.R
@@ -0,0 +1,16 @@
+## DOCUMENTATION!
+
+estimate_demand <- function(df,
+                            market_ids,
+                            market_share,
+                            outside_share,
+                            exog_charac,
+                            price,
+                            nest_shares,
+                            instruments = NULL,
+                            marketFE = "both"
+                            supply_side = FALSE
+                            ){
+
+
+}
