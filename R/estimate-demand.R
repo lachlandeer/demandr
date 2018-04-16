773e44cdd0539835ff29eacc810080df759d75d7
lachlan <lachlan.deer@uzh.ch>
2018-04-16 18:43:52 +0200

    draft functions to create a regression formula

--- R/estimate-demand.R
+++ R/estimate-demand.R
@@ -1,16 +1,67 @@
 ## DOCUMENTATION!
 
-estimate_demand <- function(df,
-                            market_ids,
+# estimate_demand <- function(df,
+#                             market_ids,
+#                             market_share,
+#                             outside_share,
+#                             exog_charac,
+#                             price,
+#                             nest_shares,
+#                             instruments = NULL,
+#                             marketFE = "both"
+#                             supply_side = FALSE
+#                             ){
+#
+#
+# }
+
+create_lhs <- function(exog_charac, price, nest_shares){
+    lhs_variables <- cbind(exog_charac, price, nest_shares)
+    lhs_formula   <- paste(lhs_variables, collapse = "+")
+    return(lhs_formula)
+}
+
+create_fe <- function(market_ids, market_fe = "both"){
+
+    if(marketFE = "both"){
+        mkt_fe <- unlist(market_ids)
+        mkt_fe <- paste(mkt_fe, collapse = "+")
+        return(mkt_fe)
+    } else if(marketFE = "geog") {
+        mkt_fe <- market_ids$[[geog_id]]
+        return(mkt_fe)
+    } else if (marketFE = "time"){
+        mkt_fe <- market_ids$[[time_id]]
+        return(mkt_fe)
+    }
+
+}
+
+create_rhs <- function(mkt_share, out_share){
+    log_mktshare <- paste0("log(", mkt_share, ")")
+    log_outshare <- paste0("log(", out_share, ")")
+
+    dep_var <- paste(log_mktshare, log_outshare, collapse = "-")
+    return(dep_var)
+}
+
+create_equation <- function(market_ids,
                             market_share,
                             outside_share,
                             exog_charac,
                             price,
                             nest_shares,
                             instruments = NULL,
-                            marketFE = "both"
-                            supply_side = FALSE
-                            ){
+                            marketFE = "both"){
 
+
+     y <- create_rhs(market_share, outside_share)
+     lhs_charac <- create_lhs(exog_charac, price, nest_shares)
+     create_fe <- function(market_ids, market_fe = marketFE)
+
+    if (is.null(instruments)){
+        est_eq <- paste(dep_var, lhs_formula, sep = "~")
+        est_eq_fe <- as.formula(paste(est_eq, market_fe, sep = "|"))
+    }
 
 }
