fa1484c862522686f872fd13060e2f8fed278ec8
lachlan <lachlan.deer@uzh.ch>
2018-08-01 19:40:09 +0200

    docs and pkg down for main regression function

--- R/estimate_demand.R
+++ R/estimate_demand.R
@@ -1,5 +1,90 @@
-## DOCUMENTATION!
-
+#' Estimate a Demand Model From Aggregate Market Shares
+#'
+#' @param df Dataframe to containing data to be used in estimation
+#' @param market_ids List of variables contaning market identifiers
+#' @param market_share Variable containing product market shares, as a character string
+#' @param outside_share Variable outside shares, as a character string
+#' @param exog_charac Vector of exogenous product characteristic variables, as a character vector
+#' @param price Variable containing market price, as a character string
+#' @param nest_shares Vector of nest share variables, as a character vector
+#' @param instruments Vector of instrument names, as a character vector - NOT CURRENTLY IMPLEMENTED
+#' @param marketFE Character string of which market fixed effects to contain,
+#'        takes value "both", "geog" or "time"
+#' @param supply_side Logical variable, \code{TRUE} leads to estimation with the
+#'        constraints from a Bertrand-Nash Oligopoly Model - NOT CURRENTLY IMPLEMENTED
+#'
+#' @return Returns an object of class "lm" when instruments = FALSE
+#'
+#' @export
+#'
+#' @examples
+#' # Regression With Two Level Nest
+#' mkts <- create_markets(geog_id = "market_id", time_id = "year")
+#' price_var     <- c("price_euro")
+#' exog_var      <- c("kilowatts", "fueleff_avg", "width", "height", "domestic")
+#' mkt_share     <- c("mkt_share")
+#' outside_share <- c("outside_share")
+#' ## Create Shares
+#' df   <- mkt_share_from_sales(eurocars,
+#'                                quantity   = 'qty_sold',
+#'                                population = 'population')
+#' df2  <- create_shares(df, market_id  = mkts,
+#'                           mkt_share  = 'mkt_share')
+#' ## Run regression
+#' reg <- estimate_demand(df             = df2,
+#'                         market_ids    = mkts,
+#'                         market_share  = mkt_share,
+#'                         outside_share = outside_share,
+#'                         exog_charac   = exog_var,
+#'                         price         = price_var,
+#'                         marketFE      = "both")
+#'
+#' # Regression With One Level Nest
+#' mkts <- create_markets(geog_id = "market_id", time_id = "year")
+#' price_var     <- c("price_euro")
+#' exog_var      <- c("kilowatts", "fueleff_avg", "width", "height", "domestic")
+#' mkt_share     <- c("mkt_share")
+#' outside_share <- c("outside_share")
+#' ## Create Shares
+#' df   <- mkt_share_from_sales(eurocars,
+#'                                quantity   = 'qty_sold',
+#'                                population = 'population')
+#' df2  <- create_shares(df, market_id  = mkts,
+#'                           mkt_share  = 'mkt_share',
+#'                           nest_id    = 'class_id')
+#' ## Run regression
+#' reg <- estimate_demand(df             = df2,
+#'                         market_ids    = mkts,
+#'                         market_share  = mkt_share,
+#'                         outside_share = outside_share,
+#'                         exog_charac   = exog_var,
+#'                         price         = price_var,
+#'                         nest_shares   = c("within_nest"),
+#'                         marketFE      = "both")
+#'
+#' # Regression With Two Level Nest
+#' mkts <- create_markets(geog_id = "market_id", time_id = "year")
+#' price_var     <- c("price_euro")
+#' exog_var      <- c("kilowatts", "fueleff_avg", "width", "height", "domestic")
+#' mkt_share     <- c("mkt_share")
+#' outside_share <- c("outside_share")
+#' ## Create Shares
+#' df   <- mkt_share_from_sales(eurocars,
+#'                                quantity   = 'qty_sold',
+#'                                population = 'population')
+#' df2  <- create_shares(df, market_id  = mkts,
+#'                           mkt_share  = 'mkt_share',
+#'                           nest_id    = 'class_id',
+#'                           subnest_id = 'domestic')
+#' ## Run regression
+#' reg <- estimate_demand(df             = df2,
+#'                         market_ids    = mkts,
+#'                         market_share  = mkt_share,
+#'                         outside_share = outside_share,
+#'                         exog_charac   = exog_var,
+#'                         price         = price_var,
+#'                         nest_shares   = c("within_nest", "within_subnest"),
+#'                         marketFE      = "both")
 estimate_demand <- function(df,
                             market_ids,
                             market_share,
@@ -50,14 +135,17 @@ create_fe <- function(market_ids, market_fe = "both"){
 
     if(market_fe == "both"){
         mkt_fe <- unlist(market_ids)
-        mkt_fe <- lapply("as.factor(", paste, unlist(mkt_ids), ")", sep = "")[[1]]
+        mkt_fe <- lapply("as.factor(", paste, unlist(market_ids), ")",
+                        sep = "")[[1]]
         mkt_fe <- paste(mkt_fe, collapse = "+")
         return(mkt_fe)
     } else if(market_fe == "geog") {
-        mkt_fe <- lapply("as.factor(", paste, unlist(mkt_ids$geog_id), ")", sep = "")[[1]]
+        mkt_fe <- lapply("as.factor(", paste, unlist(market_ids$geog_id), ")",
+                        sep = "")[[1]]
         return(mkt_fe)
     } else if (market_fe == "time"){
-        mkt_fe <- lapply("as.factor(", paste, unlist(mkt_ids$time_id), ")", sep = "")[[1]]
+        mkt_fe <- lapply("as.factor(", paste, unlist(market_ids$time_id), ")",
+                        sep = "")[[1]]
         return(mkt_fe)
     }
 
