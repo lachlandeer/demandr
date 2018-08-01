432fdfc548a9f0f71160f47b098a39d3a9fce9d1
lachlan <lachlan.deer@uzh.ch>
2018-08-01 15:53:31 +0200

    docs for create markets

--- R/create_markets.R
+++ R/create_markets.R
@@ -1,11 +1,20 @@
-# this function maps market ids into a list so they are
-# consistently unpacked
+#' Add together two numbers.
+#'
+#' @param geog_id Variable that represents geographical market identifier as a string.
+#' @param time_id Variable that represents time identifier as a string.
+#' @return A list containing \code{geog_id} and \code{time_id}.
+#'
+#' @examples
+#' # cross section data
+#' create_markets(geog_id = "state", time_id = NULL)
+#' # time series data
+#' create_markets(geog_id = NULL, time_id = "year")
+#' # panel data
+#' create_markets(geog_id = "state", time_id = "year")
 
 create_markets <- function(geog_id, time_id){
     markets <- list()
-
     markets[["geog_id"]] <- geog_id
     markets[["time_id"]] <- time_id
-
     return(markets)
 }
